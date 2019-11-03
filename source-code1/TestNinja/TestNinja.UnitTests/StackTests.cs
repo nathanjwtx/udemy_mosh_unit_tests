bd39b955fa443db2132e9a22b1cf67a9a9b6e829
Nathan <nathanjwtx@nathanjwtx.io>
2019-11-03 08:29:39 -0600

    Added additional Stack unit tests

--- source-code1/TestNinja/TestNinja.UnitTests/StackTests.cs
+++ source-code1/TestNinja/TestNinja.UnitTests/StackTests.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using TestNinja.Fundamentals;
+using Assert = NUnit.Framework.Assert;
 
 namespace TestNinja.UnitTests
 {
@@ -25,7 +28,57 @@ namespace TestNinja.UnitTests
         [Test]
         public void Push_Three_Strings_To_New_Stack_Return_Count_Of_3()
         {
+            List<string> strings = new List<string>{"hello nathan", "this is a longer string", "wibble"};
 
+            Fundamentals.Stack<string> testStack = new Fundamentals.Stack<string>();
+
+            foreach (string s in strings)
+            {
+                testStack.Push(s);
+            }
+
+            Assert.That(testStack.Count, Is.EqualTo(strings.Count));
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Push_No_Value_Expected_Throws_Error()
+        {
+            Fundamentals.Stack<string> strings = new Fundamentals.Stack<string>();
+
+            Assert.Throws<ArgumentNullException>(() => strings.Push(null));
+        }
+
+        [Test]
+        public void Pop_Returns_Last_Value_In_Stack()
+        {
+            Fundamentals.Stack<int> numbers = new Fundamentals.Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+
+            Assert.AreEqual(numbers.Pop(), 3);
+        }
+        
+        [Test]
+        public void Pop_Throws_InvalidOperationException_When_Called_On_Empty_Stack()
+        {
+            Fundamentals.Stack<int> numbers = new Fundamentals.Stack<int>();
+
+            Assert.That(() => numbers.Pop(), Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Pop_Removes_Popped_Item_From_List()
+        {
+            Fundamentals.Stack<int> numbers = new Fundamentals.Stack<int>();
+            numbers.Push(1);
+            numbers.Push(2);
+            numbers.Push(3);
+
+            numbers.Pop();
+            
+            Assert.AreEqual(numbers.Count, 2);
         }
     }
 }
