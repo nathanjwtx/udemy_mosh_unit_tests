3e7b88f6fe77a91d348d74b147835e5821cc9e54
Nathan <nathanjwtx@nathanjwtx.io>
2019-11-05 08:49:38 -0600

    Introduced Moq mocking framework

--- source-code1/TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ source-code1/TestNinja/TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using NUnit.Framework.Internal;
 using TestNinja.Mocking;
 
@@ -7,10 +8,20 @@ namespace TestNinja.UnitTests.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FakeFileReader());
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
             
             //injection by property
             //service.FileReader = new FakeFileReader();
@@ -18,7 +29,7 @@ namespace TestNinja.UnitTests.Mocking
             //injection by method parameter
 //            var result = service.ReadVideoTitle(new FakeFileReader());
 
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
             
             Assert.That(result, Does.Contain("error").IgnoreCase);
         }
